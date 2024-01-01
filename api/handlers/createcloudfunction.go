3c37f30372b5fe6991fd106f4fa8630c9b9862de
Fabricio <fabriciopolicarpo0@gmail.com>
2024-01-01 18:19:50 -0500

    done migrating to gin

---
+++ api/handlers/createcloudfunction.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"generatorv/cmds"
+	"generatorv/pkgs"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleCreateCloudFunction(c *gin.Context) {
+	var data struct {
+		Provider string               `json:"provider"`
+		Lambdas  []pkgs.CloudFunction `json:"lambdas"`
+	}
+
+	if err := c.BindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// ResourcesString
+	resourcesString, err := cmds.CreateCloudFunction(data.Lambdas, data.Provider)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"resourcesString": resourcesString})
+
+}
--- api/main.go
+++ api/main.go
@@ -2,109 +2,14 @@
 package main
 
 import (
-	"generatorv/cmds"
+	"generatorv/api/handlers"
+	_ "generatorv/api/handlers"
 	_ "generatorv/docs"
-	"generatorv/pkgs"
-	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
-
-// handleCreateBuckets godoc
-// @Summary Create S3 buckets
-// @Description Create buckets based on the provided configuration
-// @Tags buckets
-// @Accept json
-// @Produce json
-// @Param buckets body []pkgs.Bucket true "Array of Bucket Configurations"
-// @Success 200 {string} string "Buckets created successfully"
-// @Failure 400 {string} string "Bad Request"
-// @Failure 405 {string} string "Method Not Allowed"
-// @Failure 500 {string} string "Internal Server Error"
-// @Router /createbuckets [post]
-func handleCreateBuckets(c *gin.Context) {
-
-	var data struct {
-		Buckets []pkgs.Bucket `json:"buckets"`
-	}
-
-	if err := c.BindJSON(&data); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Call the CreateBuckets function from cmds package
-	resourcesString, err := cmds.CreateBuckets(data.Buckets)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-
-	c.JSON(http.StatusOK, gin.H{"resourcesString": resourcesString})
-
-}
-
-func handleCreateProvider(c *gin.Context) {
-
-	var provider pkgs.Prov
-	if err := c.BindJSON(&provider); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	providerString, err := cmds.CreateProvider(provider)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-	variablesString, err := cmds.CreateVariables(provider.Provider)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-
-	c.JSON(http.StatusOK, gin.H{"providerString": providerString, "variablesString": variablesString})
-}
-
-func handleCreateVirtualPrivateCloud(c *gin.Context) {
-
-	var data struct {
-		Vpcs []pkgs.VPC `json:"vpcs"`
-	}
-
-	if err := c.BindJSON(&data); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	resourceString, err := cmds.CreateVirtualPrivateCloud(data.Vpcs)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-
-	c.JSON(http.StatusOK, gin.H{"resourceString": resourceString})
-
-	// Call the CreateBuckets function from cmds package
-
-}
-
-func handleCreateCloudFunction(c *gin.Context) {
-	var data struct {
-		Provider string               `json:"provider"`
-		Lambdas  []pkgs.CloudFunction `json:"lambdas"`
-	}
-
-	if err := c.BindJSON(&data); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	// ResourcesString
-	resourcesString, err := cmds.CreateCloudFunction(data.Lambdas, data.Provider)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-
-	c.JSON(http.StatusOK, gin.H{"resourcesString": resourcesString})
-
-}
 
 func main() {
 	router := gin.Default()
@@ -113,10 +18,12 @@ func main() {
 	config.AllowMethods = []string{"GET", "POST", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
 	router.Use(cors.New(config))
-	router.POST("/cloudfunction", handleCreateCloudFunction)
-	router.POST("/createvpcs", handleCreateVirtualPrivateCloud)
-	router.POST("/createprovider", handleCreateProvider)
-	router.POST("/createbuckets", handleCreateBuckets)
+
+	router.POST("/cloudfunction", handlers.HandleCreateCloudFunction)
+	router.POST("/createvpcs", handlers.HandleCreateVirtualPrivateCloud)
+	router.POST("/createprovider", handlers.HandleCreateProvider)
+	router.POST("/createbuckets", handlers.HandleCreateBuckets)
 	router.Run(":8080") // or another port of your choice
+	router.GET("/swagger/*any", gin.WrapH(httpSwagger.WrapHandler))
 
 }
